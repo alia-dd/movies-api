bd2c93df2ca00bbea4e7f882b46cec068d236c42
Yeabsira Abate Guangul <k-yguangu@c1r6p8.hive.fi>
2026-08-07 15:39:57 +0300

    done CRUD

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,53 @@
-package cmd
+package main
+
+import (
+	"fmt"
+	"log"
+	"moviesApi/internal/database"
+	"moviesApi/internal/models"
+	"moviesApi/internal/repository"
+	// "time"
+)
+
+func main() {
+	db, err := database.OpenDB("movies.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	err = database.NewTable(db)
+	if err != nil {
+		log.Fatal(err)
+
+	}
+	fmt.Println("table created")
+
+	repo := repository.NewActorRepository(db)
+
+	actor := &models.Actor{
+		Name:      "whaaaat Hanks",
+		BirthDate: "1956-07-09",
+	}
+
+	err = repo.CreateActor(actor)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Before Update: %+v\n", actor)
+
+	err = repo.DeleteActors(5)
+	// actor.Name = "Yeab Hanks"
+
+	// err = repo.Update(actor)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// updatedActor, err := repo.FindById(actor.Id)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// fmt.Printf("After Update: %+v\n", actor)
+}
