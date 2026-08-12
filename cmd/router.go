4a09324cb0473142046bb7f146c3739128c84669
Yeabsira Abate Guangul <k-yguangu@c1r6p16.hive.fi>
2026-08-12 17:52:40 +0300

    change PUT to PATCH

--- cmd/router.go
+++ cmd/router.go
@@ -32,7 +32,7 @@ func route() (http.Handler, error) {
 	actorHandler := handler.NewActorHandler(actorService)
 
 	mux.HandleFunc("POST /api/actors", actorHandler.CreateActor)
-	mux.HandleFunc("PUT /api/actors/{id}", actorHandler.UpdateActor)
+	mux.HandleFunc("PATCH /api/actors/{id}", actorHandler.UpdateActor)
 	mux.HandleFunc("GET /api/actors", actorHandler.GetAllActors)
 	mux.HandleFunc("GET /api/actors/{id}", actorHandler.GetActorsById)
 	mux.HandleFunc("GET /api/actors/name/{name}", actorHandler.GetActorByName)
@@ -47,7 +47,7 @@ func route() (http.Handler, error) {
 	mux.HandleFunc("POST /api/movies", movieHandler.CreateMovie)
 	mux.HandleFunc("GET /api/movies", movieHandler.GetAllMovies)
 	mux.HandleFunc("GET /api/movies/{id}", movieHandler.GetMoviesById)
-	mux.HandleFunc("PATCH /api/movies/{id}", handler.UpdateMovie)
+	// mux.HandleFunc("PATCH /api/movies/{id}", handler.UpdateMovie)
 	mux.HandleFunc("DELETE /api/movies/{id}", movieHandler.DeleteMovie)
 	mux.HandleFunc("GET /api/movies/{movieId}/actors", movieHandler.GetActorsForMovie) // get all actor in selected movie
 
@@ -64,18 +64,3 @@ func route() (http.Handler, error) {
 	mux.HandleFunc("DELETE /api/genres/name/{name}", genreHandler.DeleteGenreByName)
 	return mux, nil
 }
-
-// Set up the following endpoints for each entity:
-
-// POST /api/{entity}: Create a new entity
-// GET /api/{entity}: Retrieve all entities
-// GET /api/{entity}/{id}: Retrieve a specific entity by ID
-// PATCH /api/{entity}/{id}: Partially update an existing entity
-// DELETE /api/{entity}/{id}: Delete an entity
-// Additionally, implement filtering endpoints for the following:
-
-// GET /api/movies?genre={genreId}: Retrieve movies filtered by genre
-// GET /api/movies?year={releaseYear}: Retrieve movies filtered by release year
-// GET /api/movies?actor={actorId}: Retrieve movies that the actor with the given id has starred in
-// GET /api/movies/{movieId}/actors: Retrieve all actors starring in a movie
-// GET /api/actors?name={name}:
